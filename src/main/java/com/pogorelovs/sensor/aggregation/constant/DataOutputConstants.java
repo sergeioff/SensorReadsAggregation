a90696a39d10714e85a7e24c356bce1b1b499e10
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-08-06 11:02:41 +0300

    feat(datasource): datasource related constants and structures

---
+++ src/main/java/com/pogorelovs/sensor/aggregation/constant/DataOutputConstants.java
@@ -0,0 +1,13 @@
+package com.pogorelovs.sensor.aggregation.constant;
+
+public interface DataOutputConstants {
+    String COL_TIME_SLOT_START = "TimeSlotStart";
+
+    String COL_TEMP_MIN = "TempMin";
+    String COL_TEMP_MAX = "TempMax";
+    String COL_TEMP_AVG = "TempAvg";
+    String COL_TEMP_COUNT = "TempCnt";
+
+    String COL_PRESENCE = "Presence";
+    String COL_PRESENCE_COUNT = "PresenceCnt";
+}
---
+++ src/main/java/com/pogorelovs/sensor/aggregation/structure/MetadataStructure.java
@@ -0,0 +1,16 @@
+package com.pogorelovs.sensor.aggregation.structure;
+
+import com.pogorelovs.sensor.aggregation.constant.DataSourceConstants;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.List;
+
+public interface MetadataStructure {
+    StructType STRUCTURE = DataTypes.createStructType(List.of(
+            DataTypes.createStructField(DataSourceConstants.COL_SENSOR_ID, DataTypes.StringType, false),
+            DataTypes.createStructField(DataSourceConstants.COL_CHANNEL_ID, DataTypes.StringType, false),
+            DataTypes.createStructField(DataSourceConstants.COL_CHANNEL_TYPE, DataTypes.StringType, false),
+            DataTypes.createStructField(DataSourceConstants.COL_LOCATION_ID, DataTypes.StringType, false)
+    ));
+}
